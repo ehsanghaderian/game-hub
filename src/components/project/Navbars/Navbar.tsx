739f564608e5aa17bdd43ad1f18a6892acf9640a
Ehsan <ehsan.ghaderian3@gmail.com>
2026-07-24 13:11:36 +0330

    remove extra imports

--- src/components/project/Navbars/Navbar.tsx
+++ src/components/project/Navbars/Navbar.tsx
@@ -1,4 +1,4 @@
-import { HStack, Text, useBreakpointValue } from "@chakra-ui/react";
+import { HStack, Text } from "@chakra-ui/react";
 import { ColorModeButton, useColorMode } from "../../ui/color-mode";
 import { Searchbar } from "../Searchbars/Searchbar";
 import { GiGamepad } from "react-icons/gi";
